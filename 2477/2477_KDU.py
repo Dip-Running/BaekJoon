2ed0bca580c9cf1bfa5c383faedc5f822f145610
unknown <dnpffldxjs12@gmail.com>
2023-02-01 14:07:35 +0900

    주석 추가

--- 2477/2477_KDU.py
+++ 2477/2477_KDU.py
@@ -9,15 +9,15 @@ for i in range(6):
 #print(my_list)
 biggest_area = 0
 
-for i in range(6):
+for i in range(6):  # 방향 의미 없으니까 그냥 가장 큰 넓이 찾음
     if biggest_area < my_list[i] * my_list[i-1]:
         biggest_area = my_list[i] * my_list[i-1]
-        a = i-1
+        a = i-1  # a에는 현재 인덱스 -1 저장
 
 #print(biggest_area)
 #print(a)
 
-b = my_list[a-2] * my_list[a-3]
+b = my_list[a-2] * my_list[a-3]  ## 어차피 빼야 할 값은 a에서 2칸 떨어진 곳 넓이
 biggest_area -= b
 #print(biggest_area)
 
--- 2605/2605_KDU.py
+++ 2605/2605_KDU.py
@@ -3,13 +3,15 @@ sys.stdin = open('input2605.txt')
 
 n = int(input())
 numb_lst = map(int, input().split())
-line_lst = []
-count = 1
+
+
+line_lst = []  # 저장할 리스트 생성
+count = 1      # 몇번째 인간인지 확인용
 for i in numb_lst:
-    line_lst.insert(i, count)
+    line_lst.insert(i, count)  # 뽑은 번호 위치에 count번째 인간 삽입함
     count += 1
 
-line_lst.reverse()
+line_lst.reverse()  # 반대여서 뒤집음
 
 for i in line_lst:
     print(i, end=' ')
