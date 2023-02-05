bd0f3a66baa58c4dc648316182fac7fd050a3564
YuJinUk <yujinuk5140@gmail.com>
2023-02-05 16:38:02 +0900

    10158

--- 10158/10158_YJU.py
+++ 10158/10158_YJU.py
@@ -3,8 +3,8 @@ def position():
     x, y = map(int, input().split())
     n = int(input())
 
-    x = [(x + n) % w if (x + n)//w % 2 == 0 else w - (x + n) % w]
-    y = [(y + n) % h if (y + n)//h % 2 == 0 else h - (y + n) % h]
+    x = [(x + n) % w if (x + n)//w % 2 == 0 else w - (x + n) % w] # (x+n)//w %2 == 0 : 오른쪽  1이면 왼쪽 
+    y = [(y + n) % h if (y + n)//h % 2 == 0 else h - (y + n) % h] # (y + n)//h % 2 == 0 : 위쪽 1이면 아래쪽
     return x+y
 result = position()
 print(*result)
