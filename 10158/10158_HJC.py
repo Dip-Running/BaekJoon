3a8fa7f7aa24465c11d552b2602c3617cac36822
wocjs <wocjs602@gmail.com>
2023-02-06 22:54:52 +0900

    10158_H

--- 10158/10158_HJC.py
+++ 10158/10158_HJC.py
@@ -1,5 +1,12 @@
 import sys
 sys.stdin = open('input.txt', 'r')
+#--ps) 감사합니다 진욱님--#
+import sys
+input = sys.stdin.readline
+w, h = map(int, input().split())    # size
+x, y = map(int, input().split())    # pos
+n = int(input())    # time
 
-w, h = map(int, input().split())
-p, q = map(int, input().split())
+nx = (x + n) % w if ((x + n) // w) % 2 == 0 else w - ((x + n) % w)
+ny = (y + n) % h if ((y + n) // h) % 2 == 0 else h - ((y + n) % h)
+print(nx, ny)
